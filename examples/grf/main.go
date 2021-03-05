581f4ca10d402a19f1fa7c671ca96ca6301e49c9
drgomesp <drgomesp@gmail.com>
2021-03-05 11:50:44 -0300

    Moment of glory, where a single cast might save the world

--- examples/grf/main.go
+++ examples/grf/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image/png"
+	_ "image/png"
 	"log"
 	"os"
 
@@ -15,7 +16,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	e, err := f.GetEntry(`data\sprite\npc\1_etc_01.spr`)
+	e, err := f.GetEntry(`data\sprite\ork_warrior.spr`)
 	//e, err := f.GetEntry(`data\sprite\count.spr`)
 	//e, err := f.GetEntry(`data\sprite\npc\bigfoot.spr`)
 	if err != nil {
@@ -29,46 +30,14 @@ func main() {
 
 	img := sprFile.Image(0, false, nil)
 
-	// outputFile is a File type which satisfies Writer interface
 	outputFile, err := os.Create("test.png")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Encode takes a writer interface and an image interface
-	// We pass it the File and the RGBA
 	if err = png.Encode(outputFile, img); err != nil {
 		log.Fatal(err)
 	}
 
-	// Don't forget to close files
 	outputFile.Close()
-
-	//fileName := os.Args[2]
-	//parts := strings.Split(fileName, "\\")
-	//err = ioutil.WriteFile(parts[len(parts)-1], e.Data.Bytes(), 0644)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//fileName := os.Args[2]
-	//parts := strings.Split(fileName, "\\")
-	//err = ioutil.WriteFile(parts[len(parts)-1], e.Data.Bytes(), 0644)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//
-	//for y := 0; y < h; y++ {
-	//	for x := 0; x < w; x++ {
-	//		idx1 := data[y*w+x] * 4
-	//
-	//		img.Set(idx1+0, int(data[idx2+0]), color.RGBA{})
-	//		img.Set(idx1+1, int(data[idx2+1]), color.RGBA{})
-	//		img.Set(idx1+2, int(data[idx2+2]), color.RGBA{})
-	//		img.Set(idx1+3, int(data[idx2+3]), color.RGBA{})
-	//	}
-	//}
-	//
-	//err = png.Encode(out, img)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.15
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/hajimehoshi/ebiten/v2 v2.0.6
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.6.1
+	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
 )
