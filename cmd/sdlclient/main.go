a3c39056f00d9ca57342134fa19c82e2283b8657
drgomesp <drgomesp@gmail.com>
2021-04-05 08:30:33 -0300

    Created basic version of SDL client with OpenGL context (things will get complex really fast)

---
+++ cmd/sdlclient/main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"strings"
+
+	"github.com/veandco/go-sdl2/sdl"
+
+	"github.com/go-gl/mathgl/mgl32"
+
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
+const (
+	windowWidth  = 1280
+	windowHeight = 720
+
+	vertexShaderSource = `
+		#version 330 core
+		layout(location = 0) in vec3 vp;
+		uniform mat4 mvp;
+
+		void main() {
+			gl_Position = mvp * vec4(vp, 1.0);
+		}
+	` + "\x00"
+
+	fragmentShaderSource = `
+		#version 330 core
+		out vec4 frag_colour;
+		void main() {
+			frag_colour = vec4(1.0, 1.0, 1.0, 1);
+		}
+	` + "\x00"
+)
+
+var (
+	triangle = []float32{
+		0, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+	}
+)
+
+func main() {
+	runtime.LockOSThread()
+
+	var err error
+	if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	var window *sdl.Window
+	if window, err = sdl.CreateWindow(
+		"test",
+		sdl.WINDOWPOS_UNDEFINED,
+		sdl.WINDOWPOS_UNDEFINED,
+		windowWidth,
+		windowHeight,
+		sdl.WINDOW_OPENGL,
+	); err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = window.Destroy()
+	}()
+
+	context, err := window.GLCreateContext()
+	if err != nil {
+		panic(err)
+	}
+	defer sdl.GLDeleteContext(context)
+
+	program := initOpenGL()
+
+	gl.Enable(gl.DEPTH_TEST)
+	gl.ClearDepth(1)
+	gl.DepthFunc(gl.LEQUAL)
+	gl.Viewport(0, 0, int32(windowWidth), int32(windowHeight))
+
+	gl.ClearColor(0, 0.5, 1.0, 1.0)
+	vao := makeVao(triangle)
+
+	//projection := mgl32.Perspective(
+	//	mgl32.DegToRad(45),
+	//	float32(windowWidth/windowHeight),
+	//	0.1,
+	//	100.0,
+	//)
+	projection := mgl32.Ortho(
+		-10,
+		10,
+		-10,
+		10.0,
+		0,
+		100.0,
+	)
+	view := mgl32.LookAtV(
+		mgl32.Vec3{4, 3, 3},
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{0, -1, 0},
+	)
+	model := mgl32.Ident4()
+	mvp := projection.Mul4(view).Mul4(model)
+	mvpUniform := gl.GetUniformLocation(program, gl.Str("mvp\x00"))
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		gl.UseProgram(program)
+		gl.UniformMatrix4fv(mvpUniform, 1, false, &mvp[0])
+
+		gl.BindVertexArray(vao)
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/3))
+
+		window.GLSwap()
+	}
+}
+
+// initOpenGL initializes OpenGL and returns an initialized program.
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	return prog
+}
+
+// makeVao initializes and returns a vertex array from the points provided.
+func makeVao(points []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,7 @@ module github.com/project-midgard/midgarts
 
 go 1.16
 
-replace (
-	github.com/g3n/engine => /home/drgomesp/src/engine
-)
+replace github.com/g3n/engine => /home/drgomesp/src/engine
 
 require (
 	github.com/AllenDang/giu v0.5.3
@@ -12,9 +10,13 @@ require (
 	github.com/EngoEngine/engo v1.0.5
 	github.com/RaniSputnik/lovepac v0.0.0-20190109190641-904e35f7bdd7
 	github.com/g3n/engine v0.1.1-0.20210127232258-6d0136f3444c
+	github.com/go-gl/gl v0.0.0-20210315015930-ae072cafe09d // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210311203641-62640a716d48 // indirect
+	github.com/go-gl/mathgl v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.7.0
+	github.com/veandco/go-sdl2 v0.4.5 // indirect
 	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
 	golang.org/x/text v0.3.5
 	gopkg.in/yaml.v2 v2.4.0 // indirect
