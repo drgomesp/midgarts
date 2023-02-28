f69aeb63fdc24208b946a9105e4c6ecba8fecd5f
Daniel Ribeiro <drgomesp@gmail.com>
2023-02-27 23:31:32 -0300

    added byteorder crate and started parsing grf header

--- src/fileformat/grf/mod.rs
+++ src/fileformat/grf/mod.rs
@@ -1,11 +1,15 @@
+use std::io::{Cursor, Read};
 use std::*;
+
+use byteorder::{LittleEndian, ReadBytesExt};
 
 use crate::fileformat::FromBytes;
 
 /// The GRF file header.
+#[derive(Debug, Default)]
 pub struct GrfHeader {
-    _signature: [u8; 15],
-    _encryption: [u8; 15],
+    signature: String,
+    encryption: [u8; 15],
     _file_table_offset: u32,
     _reserved_files: u32,
     _file_count: u32,
@@ -13,18 +17,44 @@ pub struct GrfHeader {
 }
 
 impl FromBytes for GrfHeader {
-    fn from_bytes(_bytes: &[u8]) -> Self {
-        todo!()
+    fn from_bytes(bytes: &[u8]) -> Self {
+        let mut reader = Cursor::new(bytes);
+
+        let mut buf = [0u8; 15];
+        reader
+            .read_exact(&mut buf)
+            .expect("should read header signature");
+
+        let mut signature = String::from_utf8_lossy(&buf).parse().unwrap();
+        assert_eq!(signature, "Master of Magic");
+
+        let encryption = [0u8; 15];
+        reader
+            .read_exact(&mut buf)
+            .expect("should read header signature");
+
+        GrfHeader {
+            signature,
+            encryption,
+            _file_table_offset: reader.read_u32::<LittleEndian>().unwrap(),
+            _reserved_files: reader.read_u32::<LittleEndian>().unwrap(),
+            _file_count: reader.read_u32::<LittleEndian>().unwrap(),
+            _version: reader.read_u32::<LittleEndian>().unwrap(),
+        }
     }
 }
 
 /// The GRF file.
+#[derive(Debug, Default)]
 pub struct GrfFile {
-    _header: GrfHeader,
+    /// The GRF header.
+    pub header: GrfHeader,
 }
 
 impl FromBytes for GrfFile {
-    fn from_bytes(_bytes: &[u8]) -> Self {
-        todo!()
+    fn from_bytes(bytes: &[u8]) -> Self {
+        let header = GrfHeader::from_bytes(bytes);
+
+        GrfFile { header }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::fs;
+
+use crate::fileformat::{grf::GrfFile, FromBytes};
+
 pub mod fileformat;
 
 fn main() {
-    println!("Hello, world!");
+    let path = "/Users/drgomesp/go/src/github.com/drgomesp/midgarts/assets/grf/data.grf";
+    let bytes = fs::read(path).unwrap_or_else(|_| panic!("failed to load archive from {path}"));
+
+    let grf = GrfFile::from_bytes(&bytes);
+
+    println!("grf = {:#?}", grf);
 }
